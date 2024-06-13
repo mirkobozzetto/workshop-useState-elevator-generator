44ae9adcacd882baf3bb62e62dbb69120a0c5bf9
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-13 15:03:51 +0200

    chore: Update Menu component styling and functionality in Menu.tsx

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,12 +1,26 @@
+"use client";
+
+import { useState } from "react";
+
 const Menu = () => {
+  const [value, setValue] = useState(40);
+
   return (
-    <>
+    <div className="flex flex-col m-6">
       <input
         type="text"
-        className="m-6 pl-4 w-1/6 h-10 file-input file-input-primary"
+        className="my-3 pl-4 w-1/6 h-5 file-input file-input-primary"
         placeholder="la vie est belle"
       />
-    </>
+      <input
+        type="range"
+        min={0}
+        max="100"
+        value={value}
+        onChange={(e) => setValue(Number(e.target.value))}
+        className="my-3 w-1/6 h-3 slider slider-primary"
+      />
+    </div>
   );
 };
 export default Menu;
